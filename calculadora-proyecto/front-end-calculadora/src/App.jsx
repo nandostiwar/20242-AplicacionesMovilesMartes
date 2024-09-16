e1f1b02e87895f906d88c73e6915bbcb7f518c98
julian <julian.moreno01@unicatolica.edu.co>
2024-09-16 17:57:14 -0500

    agregar horoscopo y calculadora

---
+++ calculadora-proyecto/front-end-calculadora/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Calculadora from './components/Calculadora'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Calculadora/>
+      <Calculadora/>
+      <Calculadora/>
+    </>
+  )
+}
+
+export default App
