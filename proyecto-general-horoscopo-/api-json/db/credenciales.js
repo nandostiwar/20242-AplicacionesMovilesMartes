f6d6ae2e743a28c6c8dfeac67f42af3974d512b5
jherak2904 <jherakp@gmail.com>
2024-09-24 13:00:25 -0500

    Se creo archivo credenciales

---
+++ proyecto-general-horoscopo-/api-json/db/credenciales.js
@@ -0,0 +1,6 @@
+// db/credenciales.js
+const usuarios = [
+    { username: 'alberto', password: 'alberto2108', role: 'user' },
+    { username: 'admin', password: 'admin2024', role: 'admin' }
+];
+module.exports = usuarios;
